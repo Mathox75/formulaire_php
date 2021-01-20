61f978b9c24ddb820c0c899b0c96dd69b888ae92
Mathox75 <74018516+Mathox75@users.noreply.github.com>
2021-01-20 15:25:45 +0100

    Update create.php

--- create.php
+++ create.php
@@ -1,16 +1,35 @@
-<?php 
+<?php
+include('db/db.php');
 
-include('db/user.php');
+$result = $db->query('SELECT * FROM clients');
 
-if(isset($_POST['firstname']) && $_POST['firstname'] !== ""){
-	
-	create($req, $db);
-	
-
-header('Location: show.php');
+if(isset($_POST['valider']) && isset($_POST['lastname']) && isset($_POST['firstname'])){
 
 
+
+$insertion = $db->prepare('INSERT INTO clients(lastname,firstname,email,peopleNumber,address,phone,birthdate,departureDate, arrivalDate, bedroom, requirements) VALUES(:lastname, :firstname, :email, :peopleNumber, :address, :phone, :birthdate, :departureDate, :arrivalDate, :bedroom, :requirements)');
+create($insertion);
+header('Location: index.php');
 }
 
-?>
+function create($insertion){
+
+
+	$insertion->bindValue(':lastname', $_POST['lastname'], PDO::PARAM_STR);
+	$insertion->bindValue(':firstname', $_POST['firstname'], PDO::PARAM_STR);
+	$insertion->bindValue(':email', $_POST['email'], PDO::PARAM_STR);
+	$insertion->bindValue(':peopleNumber', $_POST['peopleNumber'], PDO::PARAM_INT);
+	$insertion->bindValue(':address', $_POST['address'], PDO::PARAM_STR);
+	$insertion->bindValue(':phone', $_POST['phone'], PDO::PARAM_INT);
+	$insertion->bindValue(':birthdate', $_POST['birthdate'], PDO::PARAM_INT);
+	$insertion->bindValue(':departureDate', $_POST['departureDate'], PDO::PARAM_INT); 
+	$insertion->bindValue(':arrivalDate', $_POST['arrivalDate'], PDO::PARAM_INT);
+	$insertion->bindValue(':bedroom', $_POST['bedroom'], PDO::PARAM_INT);
+	$insertion->bindValue(':requirements', $_POST['requirements'], PDO::PARAM_INT);
+	$insertion->execute();
+}
+
+
+
+
 
