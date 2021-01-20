9d40726c88a260622a4751d3cf2af5d142ee46a9
Mathox75 <74018516+Mathox75@users.noreply.github.com>
2021-01-20 15:26:25 +0100

    Update user.php

--- user.php
+++ user.php
@@ -1,29 +1,46 @@
 <?php 
 
-include('db/db.php');
-
-$req = $db->query('SELECT * FROM clients');
 
 
-function create($clients, $db) {
-	$req = $db->prepare('INSERT INTO clients(lastname, firstname, email, peopleNumber, address, phone, birthdate, departureDate , arrivalDate, bedroom, requirements) VALUES (:lastname, :firstname, :email, :peopleNumber, :address, :phone, :birthdate, :departureDate, :arrivalDate, :bedroom, :requirements)');
-	$req->execute([
-        'lastname' => $_POST['lastname'],
-        'firstname' => $_POST['firstname'],
-        'email' => $_POST['email'],
-        'peopleNumber' => $_POST['peopleNumber'],
-        'address' => $_POST['address'],
-        'phone' => $_POST['phone'],
-        'birthdate' => $_POST['birthdate'],
-        'departureDate' => $_POST['departureDate'],
-        'arrivalDate' => $_POST['arrivalDate'],
-        'bedroom' => $_POST['bedroom'],
-        'requirements' => $_POST['requirements']
-    ]);
+
+
+$clients = $db->query('SELECT * FROM clients');
+
+if(isset($_POST['lastname']) && isset($_POST['firstname'])  && isset($_POST['email'])  && isset($_POST['peopleNumber'])  && isset($_POST['address'])  && isset($_POST['phone'])  && isset($_POST['birthdate']) && isset($_POST['departureDate'])  && isset($_POST['arrivalDate'])  && isset($_POST['bedroom'])  && isset($_POST['requirements'])) {
+
+$clients = [
+                'lastname' => $_POST['lastname'],
+                'firstname' => $_POST['firstname'],
+                'email' => $_POST['email'],
+                'peopleNumber' => $_POST['peopleNumber'],
+                'address' => $_POST['address'],
+                'phone' => $_POST['phone'],
+                'birthdate' => $_POST['birthdate'],
+                'departureDate' => $_POST['departureDate'],
+                'arrivalDate' => $_POST['arrivalDate'],
+                'bedroom' => $_POST['bedroom'],
+                'requirements' => $_POST['requirements']
+];
 
 }
 
-function delete($id, $db) {
+function create($db) {
+	$req = $db->prepare('INSERT INTO clients(lastname, firstname, email, peopleNumber, address, phone, birthdate, departureDate , arrivalDate, bedroom, requirements) VALUES (:lastname, :firstname, :email, :peopleNumber, :address, :phone, :birthdate, :departureDate, :arrivalDate, :bedroom, :requirements)');
+	$req->execute([
+                'lastname' => $_POST['lastname'],
+                'firstname' => $_POST['firstname'],
+                'email' => $_POST['email'],
+                'peopleNumber' => $_POST['peopleNumber'],
+                'address' => $_POST['address'],
+                'phone' => $_POST['phone'],
+                'birthdate' => $_POST['birthdate'],
+                'departureDate' => $_POST['departureDate'],
+                'arrivalDate' => $_POST['arrivalDate'],
+                'bedroom' => $_POST['bedroom'],
+                'requirements' => $_POST['requirements']);
+}
+
+function delete($del) {
 	$del = $db->prepare('DELETE FROM clients WHERE id=:id');
 	$del->execute([
 		'id' => $_POST['id'],
@@ -41,7 +58,7 @@ function delete($id, $db) {
 	]);
 }
 
-function update($id, $db) {
+function update($upd) {
 	$upd = $db->prepare('UPDATE clients SET lastname=:lastname, firstname=:firstname, email=:email, peopleNumber=:peopleNumber, address=:address, phone=:phone, birthdate=:birthdate, departureDate=:departureDate, arrivalDate=:arrivalDate,  bedroom=:bedroom WHERE id=:id');
     $upd->execute([
         'id' => $_POST['id'],
